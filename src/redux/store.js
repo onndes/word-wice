eb6c93a3863be80bb27329263776b1621312d3b8
onndes <vladvyljotnikov@gmail.com>
2022-12-10 18:28:38 +0200

    fix - 'A non-serializable value was detected...'

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './slices/userSlice';
+import userReducer from './slices/userSlice'
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 })
